e3c0ac5b5df56fe96715ef880fefed16632912bc
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-10-25 22:05:17 -0300

    tentando fixar axios.post

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ import { register, login, logout } from "../controllers/auth.js"
 const router = express.Router()
 
 router.post("/register", register)
-router.post("/register", login)
-router.post("/register", logout)
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router
